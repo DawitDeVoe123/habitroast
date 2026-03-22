2ef093177a7badc969e32e33120cd6ff43fff268
DawitDeVoe123 <141552549+DawitDeVoe123@users.noreply.github.com>
2026-03-22 13:06:31 +0300

    added versel.json file

--- src/index.tsx
+++ src/index.tsx
@@ -1,40 +1,16 @@
-// Include Telegram UI styles first to allow our code override the package CSS.
-import '@telegram-apps/telegram-ui/dist/styles.css';
-
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { StrictMode } from 'react';
-import { retrieveLaunchParams } from '@tma.js/sdk-react';
-
-import { Root } from '@/components/Root.tsx';
-import { EnvUnsupported } from '@/components/EnvUnsupported.tsx';
-import { init } from '@/init.ts';
-
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 import './index.css';
 
-// Mock the environment in case, we are outside Telegram.
-import './mockEnv.ts';
+// Get the basename from the current path
+const basename = '/habitroast';
 
-const root = ReactDOM.createRoot(document.getElementById('root')!);
-
-try {
-  const launchParams = retrieveLaunchParams();
-  const { tgWebAppPlatform: platform } = launchParams;
-  const debug = (launchParams.tgWebAppStartParam || '').includes('debug')
-    || import.meta.env.DEV;
-
-  // Configure all application dependencies.
-  await init({
-    debug,
-    eruda: debug && ['ios', 'android'].includes(platform),
-    mockForMacOS: platform === 'macos',
-  })
-    .then(() => {
-      root.render(
-        <StrictMode>
-          <Root/>
-        </StrictMode>,
-      );
-    });
-} catch (e) {
-  root.render(<EnvUnsupported/>);
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
