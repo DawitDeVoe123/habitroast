19112e935e716d5d12f9461a708e53926b939b94
DawitDeVoe123 <141552549+DawitDeVoe123@users.noreply.github.com>
2026-03-22 14:11:37 +0300

    disappearing problem fix1

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,15 @@
 import { Navigate, Route, Routes, HashRouter } from 'react-router-dom';
-import { useLaunchParams, useSignal, miniApp } from '@tma.js/sdk-react';
 import { AppRoot } from '@telegram-apps/telegram-ui';
 
 import { routes } from '@/navigation/routes.tsx';
 
 export function App() {
-  const lp = useLaunchParams();
-  const isDark = useSignal(miniApp.isDark);
-
   return (
-    <AppRoot
-      appearance={isDark ? 'dark' : 'light'}
-      platform={['macos', 'ios'].includes(lp.tgWebAppPlatform) ? 'ios' : 'base'}
-    >
+    <AppRoot appearance="dark" platform="base">
       <HashRouter>
         <Routes>
           {routes.map((route) => <Route key={route.path} {...route} />)}
-          <Route path="*" element={<Navigate to="/"/>}/>
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </HashRouter>
     </AppRoot>
