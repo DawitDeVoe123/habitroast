3480b70733edf38b29cdfed35f59eefc5ae3ecfa
DawitDeVoe123 <141552549+DawitDeVoe123@users.noreply.github.com>
2026-03-22 14:15:20 +0300

    no reason

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,18 @@
 import { Navigate, Route, Routes, HashRouter } from 'react-router-dom';
+import { useLaunchParams, useSignal, miniApp } from '@tma.js/sdk-react';
 import { AppRoot } from '@telegram-apps/telegram-ui';
 
 import { routes } from '@/navigation/routes.tsx';
 
 export function App() {
+  const lp = useLaunchParams();
+  const isDark = useSignal(miniApp.isDark);
+
   return (
-    <AppRoot appearance="dark" platform="base">
+    <AppRoot
+      appearance={isDark ? 'dark' : 'light'}
+      platform={['macos', 'ios'].includes(lp.tgWebAppPlatform) ? 'ios' : 'base'}
+    >
       <HashRouter>
         <Routes>
           {routes.map((route) => <Route key={route.path} {...route} />)}
